aeca359d05445bdb85c1300e9c351b8af6208de0
unknown <e.vernaschi@hotmail.com.ar>
2022-05-18 19:34:44 -0300

    Add basic test cases

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from tasks.models import Task
 
-# Create your tests here.
+class TaskTests(APITestCase):
+    def test_create_task(self):
+        """
+        Ensure we can create a new task object.
+        """
+        url = reverse('task-list')
+        data = {'type':'issue', 'title':'Send Message', 'description':'Let pilots'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Task.objects.count(), 1)
+        self.assertEqual(Task.objects.get().title, 'Send Message')
